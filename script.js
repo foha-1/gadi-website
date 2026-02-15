e2e5d0f0d826bdb6af94c15de6b29c9bb4ab0ed7
foha-1 <oha.o.franklin@gmail.com>
2026-02-15 19:51:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you for your interest in GADI. We will contact you shortly.");
+});
