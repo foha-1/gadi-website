661e749a505af5e01a386b526d7913d17eb45c81
foha-1 <oha.o.franklin@gmail.com>
2026-02-15 22:15:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.querySelector("form").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Thank you for your interest in GADI. We will contact you shortly.");
-});
+
